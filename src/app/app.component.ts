3c54200df1711821ed829fc73b8abb7ccb840a02
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-06-17 19:23:05 -0300

    Adding date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,14 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.scss'],
   animations: [trigger('bgbtnAnim',[transition(':leave',[animate(1000,style({opacity: 0}))])],)]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  dateTime!:Date
+ngOnInit() {
+
+timer(0, 1000).subscribe(()=>{
+  this.dateTime=new Date()
+})
+}
 loadingBG!: boolean
 bg:string= 'https://images.unsplash.com/photo-1654737178110-977669b9bcdc?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1NTQ5OTc0MA&ixlib=rb-1.2.1&q=80&w=1920'
   async changeBG(){
